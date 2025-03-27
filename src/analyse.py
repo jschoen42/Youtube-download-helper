cf84b1b19220babf3495eb9f0a9a5e11be3b89be
Jürgen Schoenemeyer <108626845+jschoen42@users.noreply.github.com>
2025-03-27 18:45:37 +0100

    moved test folder

--- src/analyse.py
+++ src/analyse.py
@@ -1,26 +1,34 @@
 """
-    © Jürgen Schoenemeyer, 27.03.2025 11:24
+    © Jürgen Schoenemeyer, 27.03.2025 18:45
 
     src/utils/analyse.py
 
     .venv/Scripts/activate
     uv run src/analyse.py
+    uv run src/analyse.py -l de
+
+    uv run src/analyse.py -l de > result-de.log > data/test/_result-de.log
+
 """
 from __future__ import annotations
 
 import sys
 
+import click
+
 from helper.analyse import analyse_json_all
 from utils.globals import BASE_PATH
 from utils.trace import Trace
 
-TEST_DIR = BASE_PATH / "data" / "test"
-LANGUAGE = "de"
+TEST_DIR = BASE_PATH / "test" / "data"
 
-def main() -> None:
+@click.command()
+@click.option("-l",  "--language", help="force audio language 'de', 'en', 'null'", default="")
+
+def main(language: str) -> None:
     Trace.set(show_caller=True, show_timestamp=False)
 
-    analyse_json_all( TEST_DIR, language = LANGUAGE )
+    analyse_json_all( TEST_DIR, language=language )
 
 if __name__ == "__main__":
     Trace.set(debug_mode=True, timezone=False)
